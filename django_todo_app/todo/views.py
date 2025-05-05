a0eef0cc91d652e3eb906fa28f2206340db378f9
Shaggy <shaguftakhan7030@gmail.com>
2025-05-05 17:18:01 +0530

    Delete django_todo_app directory

--- django_todo_app/todo/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from .models import Task
-
-def index(request):
-    tasks = Task.objects.all()
-    return HttpResponse('<br>'.join([task.title for task in tasks]))
